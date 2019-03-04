104dd3063f104d4b23ea55b4dbe09af3f53ca004
Dullfin <48214309+Dullfin@users.noreply.github.com>
2019-03-04 09:59:14 -0600

    Added Main file

---
+++ fgbMain.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const config = require("./config.json");
+
+client.on('ready', () => {
+    console.log("Connected as " + client.user.tag)
+})
+
+// Get your bot's secret token from:
+// https://discordapp.com/developers/applications/
+// Click on your application -> Bot -> Token -> "Click to Reveal Token"
+
+client.login(config.token)
